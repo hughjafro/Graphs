3f627d3f2c61857e718ac9ea073af2d954b86a32
Christopher Tolbert <cmtolbert1@gmail.com>
2019-12-13 09:21:17 -0800

    added dungeon_util for Stack, Queue

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,7 +10,7 @@ import random
 world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
-roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+roomGraph = {0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}], 12: [(4, 6), {'w': 1, 'e': 13}], 13: [(5, 6), {'w': 12, 'n': 14}], 14: [(5, 7), {'s': 13}], 15: [(2, 6), {'e': 1, 'w': 16}], 16: [(1, 6), {'n': 17, 'e': 15}], 17: [(1, 7), {'s': 16}]}
@@ -26,24 +26,27 @@ player = Player("Name", world.startingRoom)
 # Fill this out
 traversalPath = []
 graph = {}
-graph[room] = {}
-room = player.currentRoom.id
-exits = player.currentRoom.getexits()
-visited = set()
 
+# def room_dir(directions):
+#     num_exits = len(exits)
+#     i = 0
+#     # While there are still exits
+#     while i < num_exits:
+#         if graph[room][exits[i]] == '?':
+#             return exits[i]
 
-def room_dir(room, exits):
-    num_exits = len(exits)
-    i = 0
-    # While there are still exits
-    while i < num_exits:
-        if graph[room][exits[i]] == '?':
-            return exits[i]
+#         i += 1
+#     return ''
 
-        i += 1
-    return ''
+def find_dir(directions):
+    room_dir = {}
 
-# use the opposite direction to know which way to return
+    for direction in directions:
+        room_dir[directions] = '?'
+
+    return room_dir
+
+# Use the opposite direction to move from room to room
 def opposite_dir(direction):
     if direction == 'n':
         return 's'
@@ -54,11 +57,38 @@ def opposite_dir(direction):
     if direction == 'w':
         return 'e'
 
+def update_dir(visited, room1, room2, direction):
+    # If room not in list ad
+    if room1.id not in visited:
+        room_dir = find_dir(room1.getExits())
+        visited[room1.id] = find_dir
+    
+    if room2.id not in visited:
+        room_dir = find_dir(room2.getExits())
+        visited[room2.id] = find_dir
 
-def add_room(room:
-    graph[room] = {}
-    for direction in player.currentRoom.getexits():
-        graph[player.currentRoom.id][direction] = '?'
+    visited[room1.id][direction] = room2.id
+    visited[room2.id][opposite_dir[direction]] = room1.id
+
+def dft_graph(player, visited, traversalPath):
+    # Create an empty stack and push the starting vertex ID
+    s = Stack()
+    s.push(player.currentRoom) # starting_vertex, add room
+
+    # Create an empty Set to store visited vertices
+    visited_rooms = set()
+    # While the stack is not empty...
+    while s.size() > 0:
+        # Pop the first vertex
+        room = s.pop()  # from class in util.py
+        # If the vertex has not been visited...
+        if room.id not in visited_rooms:
+            # Mark it as visited
+            print(room.id)
+            visited_rooms.add(room.id)
+            # Then find &add all of its neighbors to top of the stack
+            for next_vert in self.get_neighbors(v):
+                s.push(next_vert)
 
 # TRAVERSAL TEST
 visited_rooms = set()
